2a4469c1fbd8baa6acc5b1d195d7442c53913f1b
diegoalarc <diego.alarcondiaz@gmail.com>
2021-01-15 22:57:34 +0100

    Update

--- Caret_predictionModel.R
+++ Caret_predictionModel.R
@@ -106,7 +106,7 @@ gbmImp
 png(file = './Plots/Variable_Importance_rforest.png', units = "px",
     width = 1200, height = 700)
 
-plot(gbmImp, top = 28, main = "Random Forest - Variable Importance plot")
+plot(gbmImp, top = 29, main = "Random Forest - Variable Importance plot")
 
 dev.off()
 
@@ -181,7 +181,7 @@ gbmImp
 png(file = './Plots/Variable_Importance_cforest.png', units = "px",
     width = 1200, height = 700)
 
-plot(gbmImp, top = 28, main = "Conditional Random Forests - Variable Importance plot")
+plot(gbmImp, top = 29, main = "Conditional Random Forests - Variable Importance plot")
 
 dev.off()
 
